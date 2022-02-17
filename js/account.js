e7125beec340a9ed77d1e1c8f1b50c048e901115
shuvokd <shuvokd1210@gmail.com>
2022-02-17 23:26:44 +0600

    Function added

--- js/account.js
+++ js/account.js
@@ -1,37 +1,73 @@
+// Function
+function getInputValue(inputID){
+    const x = document.getElementById(inputID);
+    const y = x.value;
+    const z = parseFloat(y);
+    return z;
+}
+function balancetk(a,b){
+    
+    const tk = document.getElementById('remainingBalance').innerText = a - b;
+    console.log(a,b);
+    return tk;
+}
 // Calculate Expences
 document.getElementById('btn-calc').addEventListener('click',function(){
     // Calculate button Rent
-    const calcExpRent = document.getElementById('rent');
-    const calcExpencesRent = calcExpRent.value;
-    const totalRentCost = parseFloat(calcExpencesRent);
-
+    const totalRentCost = getInputValue('rent');
     // Get the amount of Food
-    const calcExpFood = document.getElementById('food');
-    const calcExpencesFood = calcExpFood.value;
-    const totalFoodCost = parseFloat(calcExpencesFood);
-
+    const totalFoodCost = getInputValue('food');
     // Get the amount of Clothes
-    const calcExpClothes = document.getElementById('clothes');
-    const calcExpencesClothes = calcExpClothes.value;
-    const totalClothesCost = parseFloat(calcExpencesClothes);
+    const totalClothesCost = getInputValue('clothes');
+    
+
+    // Total Expences
+    document.getElementById('expencesAmount');
+    expencesAmount.innerText = totalRentCost + totalFoodCost + totalClothesCost;
+    const totalCost = expencesAmount.innerText;
+
+
+    // Total Salary
+    const totalSalary = getInputValue('income');
+
+    // Balance Remaining after cost
+    document.getElementById('totalBalance');
+    totalBalance.innerText = totalSalary - totalCost ;
+    
+})
+    //  Save Calcutation
+    document.getElementById('btn-save').addEventListener('click',function(){
+    // Saving Account
+    const totalPer = getInputValue('saveAmount');
+
+    // Total Amount
+    const totalSalary = getInputValue('income');
+
+    // Calculation
+    const calc = totalPer * totalSalary ;
+    const SaveTK = calc/100 ;
+
+    document.getElementById('saveAccount');
+    const totalSaveTK = saveAccount.innerText = SaveTK ; 
+
+    // Calculate button Rent
+    const totalRentCost = getInputValue('rent');
+    // Get the amount of Food
+    const totalFoodCost = getInputValue('food');
+    // Get the amount of Clothes
+    const totalClothesCost = getInputValue('clothes');
 
     // Total Expences
     document.getElementById('expencesAmount');
     expencesAmount.innerText = totalRentCost + totalFoodCost + totalClothesCost;
     const totalCost = expencesAmount.innerText
 
-    // Balance Update
-    // Total Salary
-    const calcSal = document.getElementById('income');
-    const calcSalary = calcSal.value;
-    const totalSalary = parseFloat(calcSalary);
     // Balance Remaining after cost
     document.getElementById('totalBalance');
-    totalBalance.innerText = totalSalary - totalCost ;
+    const balance = totalBalance.innerText = totalSalary - totalCost ;
+    balancetk(balance,totalSaveTK);
+
+
     
 })
-//  Save Calcutation
-document.getElementById('btn-save').addEventListener('click',function(){
-    
-    
-})
+
