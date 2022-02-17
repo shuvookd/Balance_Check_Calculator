a207c51a33dc696c3af6995066e5cddbc0030375
shuvokd <shuvokd1210@gmail.com>
2022-02-17 19:20:01 +0600

    Finish calculate Expences

--- js/account.js
+++ js/account.js
@@ -1,45 +1,31 @@
-// Calculate button Rent
-document.getElementById('btn-calc').addEventListener('click', function(){
-    // Get the amount of rent
+// Calculate Expences
+document.getElementById('btn-calc').addEventListener('click',function(){
+    // Calculate button Rent
     const calcExpRent = document.getElementById('rent');
     const calcExpencesRent = calcExpRent.value;
-    // console.log(calcExpencesRent);
+    const totalRentCost = parseFloat(calcExpencesRent);
 
-    const expRent = document.getElementById('expencesAmount');
-    expRent.innerText = calcExpencesRent;
-
-})
-
-// Calculate button Food
-document.getElementById('btn-calc').addEventListener('click', function(){
     // Get the amount of Food
     const calcExpFood = document.getElementById('food');
     const calcExpencesFood = calcExpFood.value;
-    // console.log(calcExpencesFood);
+    const totalFoodCost = parseFloat(calcExpencesFood);
 
-    const expFood = document.getElementById('expencesAmount');
-    expFood.innerText = calcExpencesFood;
-
-})
-
-// Calculate button Clothes
-document.getElementById('btn-calc').addEventListener('click', function(){
     // Get the amount of Clothes
     const calcExpClothes = document.getElementById('clothes');
     const calcExpencesClothes = calcExpClothes.value;
-    // console.log(calcExpencesClothes);
+    const totalClothesCost = parseFloat(calcExpencesClothes);
 
-    const expClothes = document.getElementById('expencesAmount');
-    expClothes.innerText = calcExpencesClothes;
-
-    
-
-    
-
+    // Total Expences
+    document.getElementById('expencesAmount');
+    expencesAmount.innerText = totalRentCost + totalFoodCost + totalClothesCost;
 })
-// Calculate Total Expences
-
-
-
-
 // Balance Update
+// Total Salary
+const calcSal = document.getElementById('income');
+const calcSalary = calcSal.innerText;
+const totalSalary = parseFloat(calcSalary);
+
+const totalexpences = document.getElementById('expencesAmount');
+const totalCost = expencesAmount.innerText
+
+totalBalance.innerText = totalSalary - totalCost ;
